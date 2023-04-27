c657682139b5646712a8d5edead86bc77ebafdf7
XVII <karshibaevnurmuhammad@gmail.com>
2023-04-27 23:02:41 +0500

    uploaded

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function changeCircleColor(anything) {
+    const circle = document.querySelector('circle')
+    circle.style.backgroundColor = anything;    
+} 
+
+function name(params) {
+    
+}
